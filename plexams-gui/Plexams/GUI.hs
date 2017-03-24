1497e473972b1494ae4ba680350b4c766ffb765c
Felician Pinkert <pinkert@hm.edu>
2017-03-24 12:59:05 +0100

    ADDED: simple threepenny-gui button example.

--- plexams-gui/Plexams/GUI.hs
+++ plexams-gui/Plexams/GUI.hs
@@ -1,6 +1,20 @@
 module Plexams.GUI where
 
-import           Plexams.Types
+import                Plexams.Types
+import qualified      Graphics.UI.Threepenny       as UI 
+import                Graphics.UI.Threepenny.Core
 
 mainGUI :: IO ()
-mainGUI = putStrLn "I am the GUI!"
+mainGUI = do
+   startGUI defaultConfig
+       { jsPort       = Just 8023
+       , jsStatic     = Just "../wwwroot"
+       } setup
+
+setup :: Window -> UI ()
+setup window = do
+  _ <- return window # set UI.title "Hello World!"
+  button  <- UI.button # set UI.text "Click me!"
+  _ <- getBody window #+ [element button]
+  on UI.click button $ const $ do
+      element button # set UI.text "I have been clicked!"
