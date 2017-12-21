971913cd3f1818d32888442e74968a2466f8d2f7
Oliver Braun <ob@cs.hm.edu>
2017-12-21 16:18:50 +0100

    let plexams-cli use new importPlan

---
+++ plexams-cli/app/plexams/Plexams.hs
@@ -0,0 +1,19 @@
+module Main where
+
+import           Data.Text             (unpack)
+import           Plexams.CLI.Commands
+import           Plexams.CLI.Config
+-- import           Plexams.CLI.PlanManip
+import           Plexams.CLI.Types
+import           Plexams.Import        (importPlan)
+
+main :: IO ()
+main = configmain main'
+
+main' :: Config -> IO ()
+main' config = do
+  (maybePlan, messages) <- importPlan
+  mapM_ (putStrLn . unpack) messages
+  case maybePlan of
+    Nothing    -> putStrLn "error: no plan"
+    Just plan' -> runCommand (optCommand config) config plan'
