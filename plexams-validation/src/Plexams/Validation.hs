9c1a3db33545b348019c565cc3a63284ab2b0c20
Oliver Braun <ob@cs.hm.edu>
2019-11-28 16:21:18 +0100

    remove validateLecturersMax3ExamDays

--- plexams-validation/src/Plexams/Validation.hs
+++ plexams-validation/src/Plexams/Validation.hs
@@ -3,17 +3,17 @@
 module Plexams.Validation
   ( validate
   , module Plexams.Validation.Exports
-  ) where
+  )
+where
 
-import Control.Monad.Writer
-import Data.List (nub)
-import Data.Text (append)
+import           Control.Monad.Writer
+-- import           Data.List                      ( nub )
+-- import           Data.Text                      ( append )
+-- import           TextShow                       ( showt )
 
-import TextShow (showt)
-
-import Plexams.Query
-import Plexams.Types
-import Plexams.Validation.Exports
+-- import           Plexams.Query
+import           Plexams.Types
+import           Plexams.Validation.Exports
 import qualified Plexams.Validation.Invigilation
 import qualified Plexams.Validation.Rooms
 import qualified Plexams.Validation.ScheduledExams
@@ -21,48 +21,45 @@ import qualified Plexams.Validation.ScheduledExams
 validate :: [ValidateWhat] -> Plan -> (ValidationResult, [ValidationRecord])
 validate validateWhat' = runWriter . validate' validateWhat'
 
-validate' ::
-     [ValidateWhat] -> Plan -> Writer [ValidationRecord] ValidationResult
+validate'
+  :: [ValidateWhat] -> Plan -> Writer [ValidationRecord] ValidationResult
 validate' validateWhat' plan = do
   tell [ValidationRecord Info "# Validation"]
-  scheduledExams' <-
-    if ValidateSchedule `elem` validateWhat'
-      then do
-        scheduledExams'' <- Plexams.Validation.ScheduledExams.validate plan
-        max3days <- validateLecturersMax3ExamDays plan
-        return $ validationResult [scheduledExams'', max3days]
-      else return EverythingOk
-  roomsOk <-
-    if ValidateRooms `elem` validateWhat'
-      then Plexams.Validation.Rooms.validate plan
-      -- tell [Info "no validation of rooms requested"]
-      else return EverythingOk
-  invigilationsOk <-
-    if ValidateInvigilation `elem` validateWhat'
-      then Plexams.Validation.Invigilation.validate plan
-      -- tell [Info "no validation of invigilation requested"]
-      else return EverythingOk
+  scheduledExams' <- if ValidateSchedule `elem` validateWhat'
+    then do
+      scheduledExams'' <- Plexams.Validation.ScheduledExams.validate plan
+      -- max3days         <- validateLecturersMax3ExamDays plan
+      return $ validationResult [scheduledExams'']
+    else return EverythingOk
+  roomsOk <- if ValidateRooms `elem` validateWhat'
+    then Plexams.Validation.Rooms.validate plan
+-- tell [Info "no validation of rooms requested"]
+    else return EverythingOk
+  invigilationsOk <- if ValidateInvigilation `elem` validateWhat'
+    then Plexams.Validation.Invigilation.validate plan
+-- tell [Info "no validation of invigilation requested"]
+    else return EverythingOk
   tell [ValidationRecord Info "# no more validations implemented yet"]
   return $ validationResult [scheduledExams', roomsOk, invigilationsOk]
 
-validateLecturersMax3ExamDays ::
-     Plan -> Writer [ValidationRecord] ValidationResult
-validateLecturersMax3ExamDays plan = do
-  let lecturerWithMoreThan3ExamDays =
-        filter ((> 3) . length . nub . snd) $ lecturerExamDays plan
-      ok = null lecturerWithMoreThan3ExamDays
-  tell [ValidationRecord Info "# Checking amount of exam days for each lecturer (hard)"]
-  unless ok $
-    mapM_
-      (\(l, d) ->
-         tell
-           [ ValidationRecord HardConstraintBroken $
-             "- More than 3 days of exams: " `append` showt (personShortName l) `append`
-             ": " `append`
-             showt d
-           ])
-      lecturerWithMoreThan3ExamDays
-  return $
-    if ok
-      then EverythingOk
-      else SoftConstraintsBroken
+-- validateLecturersMax3ExamDays ::
+--      Plan -> Writer [ValidationRecord] ValidationResult
+-- validateLecturersMax3ExamDays plan = do
+--   let lecturerWithMoreThan3ExamDays =
+--         filter ((> 3) . length . nub . snd) $ lecturerExamDays plan
+--       ok = null lecturerWithMoreThan3ExamDays
+--   tell [ValidationRecord Info "# Checking amount of exam days for each lecturer (hard)"]
+--   unless ok $
+--     mapM_
+--       (\(l, d) ->
+--          tell
+--            [ ValidationRecord HardConstraintBroken $
+--              "- More than 3 days of exams: " `append` showt (personShortName l) `append`
+--              ": " `append`
+--              showt d
+--            ])
+--       lecturerWithMoreThan3ExamDays
+--   return $
+--     if ok
+--       then EverythingOk
+--       else SoftConstraintsBroken
